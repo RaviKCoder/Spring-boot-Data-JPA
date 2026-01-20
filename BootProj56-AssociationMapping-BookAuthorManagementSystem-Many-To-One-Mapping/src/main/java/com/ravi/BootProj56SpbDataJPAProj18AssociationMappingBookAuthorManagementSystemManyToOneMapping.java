f249b062439fdcc9fa6c37873b95b308244cd3f8
Ravi kumar <rsravi433399@gmail.com>
2026-01-20 16:41:36 +0530

    some changes in proj56

---
+++ BootProj56-AssociationMapping-BookAuthorManagementSystem-Many-To-One-Mapping/src/main/java/com/ravi/BootProj56SpbDataJPAProj18AssociationMappingBookAuthorManagementSystemManyToOneMapping.java
@@ -0,0 +1,13 @@
+package com.ravi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootProj56SpbDataJPAProj18AssociationMappingBookAuthorManagementSystemManyToOneMapping {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootProj56SpbDataJPAProj18AssociationMappingBookAuthorManagementSystemManyToOneMapping.class, args);
+	}
+
+}
--- BootProj56-AssociationMapping-BookAuthorManagementSystem-Many-To-One-Mapping/src/test/java/com/ravi/BootProj56SpbDataJPAProj18AssociationMappingBookAuthorManagementSystemManyToOneMappingTests.java
+++ BootProj56-AssociationMapping-BookAuthorManagementSystem-Many-To-One-Mapping/src/test/java/com/ravi/BootProj56SpbDataJPAProj18AssociationMappingBookAuthorManagementSystemManyToOneMappingTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BootProj56AssociationMappingBookAuthorManagementSystemManyToOneMappingApplicationTests {
+class BootProj56SpbDataJPAProj18AssociationMappingBookAuthorManagementSystemManyToOneMappingTests {
 
 	@Test
 	void contextLoads() {
