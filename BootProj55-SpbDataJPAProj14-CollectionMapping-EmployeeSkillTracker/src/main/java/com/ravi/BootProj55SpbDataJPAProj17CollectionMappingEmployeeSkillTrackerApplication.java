1da1817ab31a7a4613f0362a7c4fc790978f56fa
Ravi kumar <rsravi433399@gmail.com>
2026-01-20 16:37:59 +0530

    some changes in proj55

--- BootProj55-SpbDataJPAProj14-CollectionMapping-EmployeeSkillTracker/src/main/java/com/ravi/BootProj55SpbDataJPAProj17CollectionMappingEmployeeSkillTrackerApplication.java
+++ BootProj55-SpbDataJPAProj14-CollectionMapping-EmployeeSkillTracker/src/main/java/com/ravi/BootProj55SpbDataJPAProj17CollectionMappingEmployeeSkillTrackerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BootProj55SpbDataJPAProj14CollectionMappingEmployeeSkillTrackerApplication {
+public class BootProj55SpbDataJPAProj17CollectionMappingEmployeeSkillTrackerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BootProj55SpbDataJPAProj14CollectionMappingEmployeeSkillTrackerApplication.class, args);
+		SpringApplication.run(BootProj55SpbDataJPAProj17CollectionMappingEmployeeSkillTrackerApplication.class, args);
 	}
 
 }
--- BootProj55-SpbDataJPAProj14-CollectionMapping-EmployeeSkillTracker/src/test/java/com/ravi/BootProj55SpbDataJPAProj17CollectionMappingEmployeeSkillTrackerApplicationTests.java
+++ BootProj55-SpbDataJPAProj14-CollectionMapping-EmployeeSkillTracker/src/test/java/com/ravi/BootProj55SpbDataJPAProj17CollectionMappingEmployeeSkillTrackerApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BootProj55SpbDataJPAProj14CollectionMappingEmployeeSkillTrackerApplicationTests {
+class BootProj55SpbDataJPAProj17CollectionMappingEmployeeSkillTrackerApplicationTests {
 
 	@Test
 	void contextLoads() {
